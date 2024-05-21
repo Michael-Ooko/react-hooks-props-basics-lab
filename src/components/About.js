fb0fb8f7a50ce3d7977f74d18050a938ef405ef1
Michael <oliengo.michael@gmail.com>
2024-05-21 06:49:54 +0300

    complete react-hooks-props-basics-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links"; // Make sure to import the Links component
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.trim() !== "" && <p>{props.bio}</p>}  {/* Conditionally render the bio */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* Pass the links props to the Links component */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
